4abfd27562566d1bd0ca330393e3b9574ab5dc06
sisiodos <sisiodos@gmail.com>
2025-04-30 11:52:29 +0900

    add: chmod flink/lib

--- scripts/00-flink-setup.sh
+++ scripts/00-flink-setup.sh
@@ -15,3 +15,5 @@ curl --create-dirs --output ${TGT} ${SRC}
 SRC=https://repo1.maven.org/maven2/org/apache/flink/flink-connector-opensearch-base/2.0.0-1.19/flink-connector-opensearch-base-2.0.0-1.19.jar
 TGT=$(pwd)/flink/lib/ext/$(basename ${SRC})
 curl --create-dirs --output ${TGT} ${SRC}
+
+chmod -R 777 ./flink/lib
