426ad72e88c17180cfdfaa563ab2c2c66fba57c3
sisiodos <sisiodos@gmail.com>
2025-07-03 17:05:49 +0900

    use flink/plugins rather than flink/lib to avoid class conflict

--- flink/jobmanager-entrypoint.sh
+++ flink/jobmanager-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[flink-jobmanager] Copying connector JARs..."
-cp /opt/flink/lib/ext/*.jar /opt/flink/lib/
+# echo "[flink-jobmanager] Copying connector JARs..."
+# cp /opt/flink/lib/ext/*.jar /opt/flink/lib/
 
 echo "[flink-jobmanager] Starting Flink JobManager..."
 exec /docker-entrypoint.sh jobmanager
--- flink/taskmanager-entrypoint.sh
+++ flink/taskmanager-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "[flink-taskmanager] Copying connector JARs..."
-cp /opt/flink/lib/ext/*.jar /opt/flink/lib/
+# echo "[flink-taskmanager] Copying connector JARs..."
+# cp /opt/flink/lib/ext/*.jar /opt/flink/lib/
 
 echo "[flink-taskmanager] Starting Flink TaskManager..."
 exec /docker-entrypoint.sh taskmanager
